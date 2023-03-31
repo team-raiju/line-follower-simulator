c6b60d786ca0cd449a2ca32ac4d2cbbd14517ffd
marcoabensur <marco.abensur@gmail.com>
2023-03-31 09:01:59 -0300

    More machine learning tests

--- ml_waypoint.py
+++ ml_waypoint.py
@@ -5,12 +5,15 @@ from robot import Robot
 import pygame
 import os
 from generate_track import Map
+from pygame.math import Vector2
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_checker import check_env
+import math
+
 
 HOME_DIR = os.path.dirname(__file__)
 
@@ -19,8 +22,8 @@ class LineFollowerEnv(Env):
 
         # Env
         self.action_space = Box(low=-1, high=1, shape=(2, ), dtype=np.float32)
-        self.observation_space = Box(low=np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), 
-                                     high=np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), dtype=np.int32)
+        self.observation_space = Box(low=np.array([0, -180, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), 
+                                     high=np.array([700, 180, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), dtype=np.int32)
 
         self.max_duration = 2000
 
@@ -45,31 +48,63 @@ class LineFollowerEnv(Env):
         self.map.gen_default_track()
 
         self.waypoint_idx = 0
+    
+    def trackGoal(self, currentPos: Vector2, goal: Vector2, robot_angle):
+        dist = currentPos.distance_to(goal)
+
+        dx = goal.x - currentPos.x
+        dy = goal.y - currentPos.y
+
+        # calculate the angle in radians and convert to degrees
+        angleDiffRaw = (math.atan2(dy, dx) * 180 / math.pi)
+
+        angleDiff = angleDiffRaw - (-robot_angle % 360)
+        if (angleDiff <= -180):
+            angleDiff += 360
+        
+        return (dist, angleDiff)
+    
+    def set_motors_voltage(self, vel, w):
+        self.robot.motor_l.set_voltage((2*vel - w) / 2) # w*d actually
+        self.robot.motor_r.set_voltage((2*vel + w) / 2) # w*d actually
         
     def step(self, action):
         
-        # Action
 
-        last_mot_vel_l = self.robot.mot_vel_l / 100.0
-        last_mot_vel_r = self.robot.mot_vel_r / 100.0
         last_line_sensor = self.robot.get_line_sensor(self.screen, self.screen_width , self.screen_height)
 
+
+        # Action
         mot_left = int(action[0] * 100)
         mot_right = int(action[1] * 100)
         self.robot.motor_l.set_voltage(mot_left)
         self.robot.motor_r.set_voltage(mot_right)
-        
         self.robot.update(self.tick_period)
-        line_sensor = self.robot.get_line_sensor(self.screen, self.screen_width , self.screen_height)
-
 
         # Observation
-        obs_np_1 = np.asarray(last_line_sensor, dtype=np.int32)
-        obs_np_2 = np.asarray(line_sensor, dtype=np.int32)
+        line_sensor = self.robot.get_line_sensor(self.screen, self.screen_width , self.screen_height)
+        (dist, angleDiff) = self.trackGoal(self.robot.position, self.map.waypoint[self.waypoint_idx], self.robot.angle)
+
+        if dist > 700:
+            dist = 700
+        
+        if (angleDiff > 180):
+            angleDiff = 180
+        elif angleDiff < -180:
+            angleDiff = -180
+
+
+        obs_np_1 = np.array([int(dist), int(angleDiff)], dtype=np.int32)
+        obs_np_2 = np.asarray(last_line_sensor, dtype=np.int32)
+        obs_np_3 = np.asarray(line_sensor, dtype=np.int32)
         obs_np = np.append(obs_np_1, obs_np_2)
+        obs_np = np.append(obs_np, obs_np_3)
+
+
+        
 
         # Reward
-        reward = -0.05
+        reward = 0
 
         if (self.map.near_waypoint(self.robot.position, self.waypoint_idx)):
                 multiplier = self.waypoint_idx
@@ -121,11 +156,12 @@ class LineFollowerEnv(Env):
         self.max_duration = 2000
         self.waypoint_idx = 0
         self.robot = Robot(20, 245, 90)
-        line_sensor = self.robot.get_line_sensor(self.screen, self.screen_width, self.screen_height)
-        obs_np = np.asarray(line_sensor, dtype=np.int32)
-        # obs_np = np.append(obs_np, [0, 0])
-        obs_np = np.append(obs_np, obs_np)
 
+        line_sensor = self.robot.get_line_sensor(self.screen, self.screen_width, self.screen_height)
+        obs_np_1 = np.array([0, 0], dtype=np.int32)
+        obs_np_2 = np.asarray(line_sensor, dtype=np.int32)
+        obs_np = np.append(obs_np_1, obs_np_2)
+        obs_np = np.append(obs_np, obs_np_2)
 
         return obs_np
 
@@ -134,14 +170,14 @@ class LineFollowerEnv(Env):
 
 
 def train_model(iterations, name):
-    PPO_Path = os.path.join(HOME_DIR, 'Models_7', name)
+    PPO_Path = os.path.join(HOME_DIR, 'Models_8', name)
     log_path = os.path.join(HOME_DIR, 'logs')
     model = PPO('MlpPolicy', env, verbose = 1, tensorboard_log=log_path)
     model.learn(total_timesteps=iterations)
     model.save(PPO_Path)
 
 def train_existing_model(model: PPO, iterations, name):
-    PPO_Path = os.path.join(HOME_DIR, 'Models_7', name)
+    PPO_Path = os.path.join(HOME_DIR, 'Models_8', name)
     model.learn(total_timesteps=iterations)
     model.save(PPO_Path)
 
@@ -161,13 +197,13 @@ def run_simulation(model : PPO, episodes):
 if __name__ == '__main__':
     env = LineFollowerEnv()
 
-    PPO_Path_Init = os.path.join(HOME_DIR, 'Models_7', 'PPO_Model_Raijin_4M')
-    model = PPO.load(PPO_Path_Init, env=env)
+    # PPO_Path_Init = os.path.join(HOME_DIR, 'Models_8', 'PPO_Model_Raijin_1M300k')
+    # model = PPO.load(PPO_Path_Init, env=env)
     
-    # train_existing_model(model, 200000, 'PPO_Model_Raijin_1M200k' )
-    # train_model(4000000, 'PPO_Model_Raijin_4M' )
+    # train_existing_model(model, 400000, 'PPO_Model_Raijin_1M300k' )
+    train_model(820000, 'PPO_Model_Raijin_820k' )
     
-    run_simulation(model, 4)
+    # run_simulation(model, 4)
 
 
     
