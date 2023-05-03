e056a5003ca57199b87ff2669f053b388218889f
marcoabensur <marco.abensur@gmail.com>
2023-05-03 08:02:53 -0300

    Organize folders

--- image_conversion/map_from_img.py
+++ image_conversion/map_from_img.py
@@ -1,13 +1,32 @@
 import cv2
+import os
 
-# Load the image
-img = cv2.imread('/home/marco/Documents/Projetos/raijin/line-follower-ml/image_conversion/rsm-2023.jpg')
+INPUT_FILE_NAME = "rsm-2023.jpg"
+OUTPUT_FILE_NAME = "out-rsm.png"
 
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Pixel value to be considered as a black pixel 0 - 255
+FILTER_THERESHOLD = 240 
 
-# Apply thresholding to convert to black and white
-_, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
 
-# Save the result to a file
-cv2.imwrite('filter-rsm-2023.jpg', thresh)
+def main():
+
+    # File Paths
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    input_image_path = os.path.join(current_dir, "img_raw", INPUT_FILE_NAME)
+    output_image_path = os.path.join(current_dir, "img_filtered", OUTPUT_FILE_NAME)
+
+    # Load the image
+    img = cv2.imread(input_image_path)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply thresholding to convert to black and white
+    _, thresh = cv2.threshold(gray, FILTER_THERESHOLD, 255, cv2.THRESH_BINARY)
+
+    # Save the result to a file
+    cv2.imwrite(output_image_path, thresh)
+
+
+if __name__ == '__main__':
+    main()
