050ca7567e36c82f3a54ece71a8bbe86f03d0cc9
marcoabensur <marco.abensur@gmail.com>
2023-06-25 17:26:12 -0300

    Improve map track

--- map_track.py
+++ map_track.py
@@ -6,18 +6,18 @@ from load_track import LoadMap
 from robot import Robot
 import math
 
-MAPPING_NAME = "map0"
+MAPPING_NAME = "map3"
 
-MAP_FILE_NAME = "map0.png"
-MAP_WIDTH_CM = 100
-MAP_HEIGHT_CM = 300
-MAP_MARGIN_CM = 10
-MAP_CM_PER_PIXELS = 3
+MAP_FILE_NAME = "map3.png"
+MAP_WIDTH_CM = 545
+MAP_HEIGHT_CM = 595
+MAP_MARGIN_CM = 25
+MAP_CM_PER_PIXELS = 1.5
 
-ROBOT_INIT_POS_X_CM = 11
-ROBOT_INIT_POS_Y_CM = 220
-ROBOT_INIT_ANGLE = 90
-MIN_LEFT_MARKER_COUNTER = 22
+ROBOT_INIT_POS_X_CM = 175
+ROBOT_INIT_POS_Y_CM = 12
+ROBOT_INIT_ANGLE = 180
+MIN_LEFT_MARKER_COUNTER = 40
 
 ROBOT_IMAGE = "robot-img.png"
 ROBOT_SIZE_X_CM = 14.0 # Width
@@ -25,6 +25,8 @@ ROBOT_SIZE_Y_CM = 14.0 # Height
 ROTATION_OFFSET_FROM_CENTER_CM = 4.73
 WHEELS_DIST_CM = 14.0
 WHEELS_RADIUS_CM = 1.0
+
+
 
 
 class Game:
@@ -68,11 +70,12 @@ class Game:
         self.last_error = 0
         
         self.base_speed = 33
-        self.kp = 5
-        self.kd = 2.5
+        self.kp = 3
+        self.kd = 1.5
 
         self.track_points = []
         self.track_markers = []
+        self.track_markers_center = []
         self.points_between_markers = []
         self.track_total_dist = []
         self.track_thetas = []
@@ -103,6 +106,11 @@ class Game:
         with open(file_path, "w") as f:
             for radius in self.radius_list:
                 f.write(f"{radius}\n")
+        
+        file_path = os.path.join(current_dir, "maps", "mapping_data", MAPPING_NAME, (MAPPING_NAME + "_markers.txt"))
+        with open(file_path, "w") as f:
+            for marker in self.track_markers:
+                f.write(f"{marker}\n")
 
 
     def gen_waypoint(self):
@@ -112,16 +120,83 @@ class Game:
         # check for circles and add waypoints right before and in the middle of circles
         with open(file_path, "w") as f:
             for i in range(len(self.radius_list)):
-                if (self.radius_list[i] < 150):
+                if (abs(self.radius_list[i]) < 150):
                     point = self.track_points[i]
                     f.write(f"{point[0]},{point[1]}\n")
+        
+            # Always write the last point
+            point = self.track_points[-1]
+            f.write(f"{point[0]},{point[1]}\n")
 
 
+
+    def gen_optimized_waypoints(self):
         
 
-    # def gen_optimized_waypoints(self):
-        
+        centers = []
+        optmized_points = []
 
+        for i in range(len(self.track_markers) - 1):
+            circle_center_x = (self.track_markers[i][0] + self.track_markers[i + 1][0]) / 2
+            circle_center_y = (self.track_markers[i][1]  + self.track_markers[i + 1][1]) / 2
+            point = (circle_center_x, circle_center_y)
+            # x_val_pixel, y_val_pixel = self.coord_cm_to_pixel(point)
+            center = Vector2(point[0], point[1])
+            centers.append(center)
+            # pygame.draw.circle(self.screen, (255, 0, 0), (x_val_pixel, y_val_pixel), 3, 0)
+
+
+        # get medium curvature between markers
+        # If circle, then aproxima o centro do centro. E a cada 2 para o lado aproxima do centro tambem
+        for i in range(len(self.points_between_markers) - 1):
+            init = self.points_between_markers[i]
+            end  = self.points_between_markers[i + 1]
+            num_of_points = max((end - init), 1)
+            radius_sum = sum(self.radius_list[init: end])
+            mean_radius = radius_sum / num_of_points
+
+            marker_center = (self.track_markers_center[i][0], self.track_markers_center[i][1])
+            optmized_points.append(marker_center)
+
+            if (mean_radius < 1000):
+                waypoints = []
+                
+                if (num_of_points <= 5 and num_of_points > 0):
+                    waypoint = self.track_points[int((init + end) / 2)]
+                    waypoints.append(waypoint)
+                else:
+                    for j in range((init + 2), end - 3, 2):
+                        waypoint = self.track_points[int(j)]
+                        waypoints.append(waypoint)
+
+
+                for item in waypoints:
+                    my_vect = Vector2(item[0], item[1])
+                    dx = my_vect.x - centers[i].x
+                    dy = my_vect.y - centers[i].y
+                    angleDiffRaw = (math.atan2(dy, dx) * 180 / math.pi)
+
+                    new_vect = my_vect - Vector2(10, 0).rotate(angleDiffRaw)
+                    optmized_points.append(new_vect)
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, "maps", "mapping_data", MAPPING_NAME, (MAPPING_NAME + "_opt_waypoints.txt"))
+
+        with open(file_path, "w") as f:
+            for optmized in optmized_points:
+                f.write(f"{optmized[0]},{optmized[1]}\n")
+                x_val_pixel, y_val_pixel = self.coord_cm_to_pixel(optmized)
+                pygame.draw.circle(self.screen, (0, 0, 255), (x_val_pixel, y_val_pixel), 3, 0)
+
+             # Always write the last point
+            point = self.track_points[-1]
+            f.write(f"{point[0]},{point[1]}\n")
+
+
+    def coord_cm_to_pixel(self, point):
+        x_val_pixel = (point[0] + MAP_MARGIN_CM) * MAP_CM_PER_PIXELS
+        y_val_pixel = (point[1] + MAP_MARGIN_CM) * MAP_CM_PER_PIXELS
+        return x_val_pixel, y_val_pixel
     
     def draw_map(self):
         self.map.draw_loaded_map()
@@ -177,6 +252,13 @@ class Game:
         finished = False
         last_dist_saved = 0
         last_theta = 0
+
+        start_left_marker = False
+        left_marker_position = Vector2(0, 0)
+        left_marker_position_center = Vector2(0, 0)
+
+        start_right_marker = False
+
         while not self.exit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -186,9 +268,9 @@ class Game:
             if (not finished):
                 time += dt 
 
-            self.screen.fill((0, 0, 0))
-            self.draw_map()
-            self.draw_timer(time)
+                self.screen.fill((0, 0, 0))
+                self.draw_map()
+                self.draw_timer(time)
 
 
             line_sensor = self.robot.get_line_sensor(
@@ -227,7 +309,7 @@ class Game:
                 radius = 5000
                 if delta_theta != 0:
                     theta_rad = math.radians(delta_theta)
-                    radius = abs((total_dist - last_dist_saved) / theta_rad)
+                    radius = (total_dist - last_dist_saved) / theta_rad
                 
                 print(radius)
                 self.radius_list.append(radius)
@@ -239,33 +321,63 @@ class Game:
             # Count markers
             left_marker = line_sensor[16] == 1 or line_sensor[17] == 1
             right_marker = line_sensor[18] == 1 or line_sensor[19] == 1
-            if (left_marker and not self.last_left_marker):
-                self.left_marker_counter += 1
-                print("Left marker - " + str(self.left_marker_counter))
+            
+            if (start_left_marker):
+                if (right_marker):
+                    start_left_marker = False
+                    print("Crossing L")
+                elif (not left_marker and self.last_left_marker):
+                    self.left_marker_counter += 1
+                    print("Left marker - " + str(self.left_marker_counter))
+                    self.track_markers.append(left_marker_position)
+                    self.track_markers_center.append(left_marker_position_center)
+                    self.points_between_markers.append(int(len(self.track_points)))
+                    start_left_marker = False
 
-                offset = Vector2(self.robot.rot_center_offset_cm, 0)
-                sensor_position = self.robot.estimated_position_cm + self.robot.line_sensor_pos[0].rotate(-self.robot.estimated_angle) + offset.rotate(-self.robot.estimated_angle)
-                self.track_markers.append(sensor_position)
-                self.points_between_markers.append(int(len(self.track_points)))
+            else:
+                if (left_marker and not self.last_left_marker and not right_marker):
+                    start_left_marker = True
+                    offset = Vector2(self.robot.rot_center_offset_cm, 0)
+                    sensor_position = self.robot.estimated_position_cm + self.robot.line_sensor_pos[16].rotate(-self.robot.estimated_angle) + offset.rotate(-self.robot.estimated_angle)
+                    sensor_position.x -= MAP_MARGIN_CM
+                    sensor_position.y -= MAP_MARGIN_CM
+                    left_marker_position = sensor_position
 
-            if (right_marker and not self.last_right_marker):
-                self.right_marker_counter += 1
-                print("Right marker - " + str(self.right_marker_counter))
+                    # Center of the robot
+                    robot_center = Vector2(self.robot.estimated_position_cm.x, self.robot.estimated_position_cm.y)
+                    robot_center.x -= MAP_MARGIN_CM
+                    robot_center.y -= MAP_MARGIN_CM
+                    left_marker_position_center = robot_center
 
-                if (self.right_marker_counter == 1 and self.left_marker_counter < 2):
-                    time = 0
 
-                if (self.left_marker_counter > MIN_LEFT_MARKER_COUNTER):
-                    print("Total time: " + str(round(time, 4)) + "s")
-                    self.save_track_mapped()
-                    self.gen_waypoint()
-                    # self.gen_optimized_waypoints()
-                    finished = True
 
-                    self.base_speed = 15
-                    self.kd = 0
-                    self.kp = 0
+            if (start_right_marker):
+                if (left_marker):
+                    start_right_marker = False
+                    print("Crossing R")
+                elif (not right_marker and self.last_right_marker):
+                    self.right_marker_counter += 1
+                    print("Right marker - " + str(self.right_marker_counter))
 
+                    if (self.right_marker_counter == 1 and self.left_marker_counter < 2):
+                        time = 0
+
+                    if (self.left_marker_counter > MIN_LEFT_MARKER_COUNTER):
+                        print("Total time: " + str(round(time, 4)) + "s")
+                        self.save_track_mapped()
+                        self.gen_waypoint()
+                        self.gen_optimized_waypoints()
+                        finished = True
+
+                        self.base_speed = 15
+                        self.kd = 0
+                        self.kp = 0
+                    
+                    start_right_marker = False
+
+            else:
+                if (right_marker and not self.last_right_marker and not left_marker):
+                    start_right_marker = True
 
 
             self.last_left_marker = left_marker
